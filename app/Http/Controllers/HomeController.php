fb8381bffa5529eb4f9b1b5783b9c58da67a9642
vagrant <vagrant@homestead>
2021-05-22 02:17:59 +0000

    再up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,9 +27,9 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $players =\DB::table('twentyFirstPlayers')->join('teams','twentyFirstPlayers.team_id','=','teams.team_id')->join('positions','twentyFirstPlayers.position_id','=','positions.position_id')->get();
-        $playerName =\DB::table('twentyFirstPlayers')->join('teams','twentyFirstPlayers.team_id','=','teams.team_id')->join('positions','twentyFirstPlayers.position_id','=','positions.position_id')->get(['name']);
-        $getPlayer = $players->random(1);
-        return view('top',compact('players','playerName','getPlayer'));
+        // $playerName =\DB::table('twentyFirstPlayers')->join('teams','twentyFirstPlayers.team_id','=','teams.team_id')->join('positions','twentyFirstPlayers.position_id','=','positions.position_id')->get(['name']);
+        // $getPlayer = $players->random(1);
+        return view('top',compact('players'));
     }
     public function edit(){
         return view('edit');
